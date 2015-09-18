37a0530bc3900a64f6b5ca4b6fbb9d7a4314b741
James Boric <jamesboric@grapevine.com.au>
2015-09-18 15:35:41 +1000

    Initial Commit

---
+++ SymphonaicTests/SymphonaicTests.swift
@@ -0,0 +1,36 @@
+//
+//  SymphonaicTests.swift
+//  SymphonaicTests
+//
+//  Created by James Boric on 18/09/2015.
+//  Copyright © 2015 Ode To Code. All rights reserved.
+//
+
+import XCTest
+@testable import Symphonaic
+
+class SymphonaicTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
